91919357040cc41155f8ee44d378cebfe59ae565
Nicholas Costa <nicholascostadev@gmail.com>
2022-09-14 12:09:00 -0300

    💄 style: Add corresponding word for titles of table

--- src/pages/admin/answers/[userCPF].tsx
+++ src/pages/admin/answers/[userCPF].tsx
@@ -30,6 +30,22 @@ import { NotAllowed } from '../../../components/NotAllowed'
 import { formatApproval } from '../../../utils/formatters'
 import { trpc } from '../../../utils/trpc'
 
+enum TABLE_TITLES_ENUM {
+  id = 'id',
+  name = 'Nome',
+  email = 'Email',
+  cpf = 'CPF',
+  cep = 'CEP',
+  age = 'Idade',
+  deficiency = 'Deficiência',
+  prev_knowledge = 'Conhecimento prévio',
+  school_level = 'Nível escolar',
+  occupation = 'Ocupação',
+  approved = 'Estado de Aprovação',
+  created_at = 'Data de registro',
+  sex = 'Sexo',
+}
+
 export default function Answers() {
   const { query } = useRouter()
   const { userCPF } = query
@@ -156,7 +172,7 @@ export default function Answers() {
                   padding="2"
                   borderColor={borderColor}
                 >
-                  {key}
+                  {TABLE_TITLES_ENUM[key as keyof RegisteredUser]}
                 </Text>
               )
             })}

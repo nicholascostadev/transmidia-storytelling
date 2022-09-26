9fb9282c85fecbd320369727c147214522d59095
Nicholas Costa <nicholascostadev@gmail.com>
2022-09-26 09:06:40 -0300

    💡 Comment: add docs to utils

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,3 +1,8 @@
+/**
+ * It takes the approval state and returns the translated state
+ * @param {boolean} approval - boolean
+ * @returns A function
+ */
 export function formatApproval(approval: boolean): string {
   return approval ? 'Aprovado' : 'Não aprovado'
 }
--- src/utils/stringOrNull.ts
+++ src/utils/stringOrNull.ts
@@ -1,4 +1,9 @@
-export function stringOrNull(str: unknown) {
+/**
+ * @param {unknown} str - unknown
+ * @returns It returns the `str` or `null` if it's not a string, used for reading
+ * the query without the option of it being other type than string
+ */
+export const stringOrNull = (str: unknown) => {
   if (typeof str === 'string') {
     return str
   }

0162d313b45e20f7c7a9f851c8530f4562a68af9
Nicholas Costa <nicholascostadev@gmail.com>
2022-11-24 15:46:54 -0300

    ♻️ Refactor cpf validation

--- @types/formValidation/index.ts
+++ @types/formValidation/index.ts
@@ -1,9 +1,14 @@
 import { z } from 'zod'
+import { validateCPF } from './cpf'
 
 export const validation = z.object({
   name: z.string().min(1, 'Nome é Obrigatório'),
   email: z.string().min(1, 'Email é Obrigatório').email(),
-  cpf: z.string().length(11, 'CPF deve ter 11 caracteres'),
+  cpf: z.string().length(11, 'CPF deve ter 11 dígitos').refine(validateCPF, {
+    message: 'CPF informado não existe',
+  }),
+  // Can be used like that for testing purposes, not in production
+  // cpf: z.string().length(11, "CPF inválido")
   city: z.string().min(1, 'Cidade deve ser informada'),
   state: z.string().min(1, 'Seu estado deve ser informado'),
   age: z
@@ -21,3 +26,5 @@ export const validation = z.object({
   academic: z.enum(['ensino-fundamental', 'ensino-medio', 'ensino-superior']),
   occupation: z.enum(['estudante', 'profissional']),
 })
+
+export type FormData = z.infer<typeof validation>
